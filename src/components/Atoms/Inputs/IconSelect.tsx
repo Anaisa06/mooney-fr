e4ab3a3e901af2e642f15ace5619943795a2adbd
Anaisa06 <isa.alzate98@gmail.com>
2024-12-17 01:03:09 -0500

    form to create category done

---
+++ src/components/Atoms/Inputs/IconSelect.tsx
@@ -0,0 +1,64 @@
+import { Theme } from '@react-navigation/native';
+import React, { useState } from 'react'
+import { FieldError } from 'react-hook-form';
+import { StyleSheet, Text, View } from 'react-native';
+import { IconButton } from 'react-native-paper'
+
+interface IProps {
+    theme: Theme;
+    items: string[];
+    setIcon: (icon: string) => void;
+    value: string;
+    error?: FieldError;
+}
+
+const IconSelect = ({ theme, items, setIcon, value, error }: IProps) => {
+
+
+    const handleIconPress = (icon: string) => {
+        setIcon(icon);
+    };
+
+    const styles = createStyles(theme);
+
+
+    return (
+        <View style={styles.iconList}>
+            {items.map((icon) => (
+                <IconButton
+                    key={icon}
+                    icon={icon}
+                    size={30}
+
+                    iconColor={value === icon ? theme.colors.primary : theme.colors.text} // Color si está seleccionado
+                    onPress={() => handleIconPress(icon)} // Actualiza el estado
+                />
+            ))}
+            {
+                error &&
+                <Text style={styles.errorText}>
+                    {error.message}
+                </Text>
+            }
+        </View>
+    )
+
+}
+
+const createStyles = (theme: Theme) =>
+    StyleSheet.create({
+
+        iconList: {
+            flexDirection: 'row',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+        },
+        errorText: {
+            color: 'red',
+            fontSize: 10,
+            textAlign: 'center',
+            letterSpacing: 1
+        },
+    });
+
+export default IconSelect
